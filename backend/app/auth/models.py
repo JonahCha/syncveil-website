04e96c2eadfc70d9fd859e9b9a1d7268e23eb99e
Jonha Chawre <jonhachawre@gmail.com>
2026-06-04 16:04:10 +0530

    Delete backend directory
    Signed-off-by: Jonha Chawre <jonhachawre@gmail.com>

--- backend/app/auth/models.py
+++
@@ -1,7 +0,0 @@
-"""Backwards-compatible aliases for auth code."""
-from app.db.models import EmailVerification, Session, User
-
-EmailVerificationToken = EmailVerification
-RefreshToken = Session
-
-__all__ = ["User", "EmailVerificationToken", "RefreshToken"]
